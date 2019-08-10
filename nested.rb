238cb3b32beeb23fe6fa5d0a4da65b86137b2c2f
sami suliman <sami.bco@gmail.com>
2019-08-10 05:06:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -19,9 +19,9 @@ def hopper
           :languages => ["C"]
         }
      }
-puts programmer_hash[:grace_hopper]
 
 end
+puts programmer_hash[:grace_hopper]
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
